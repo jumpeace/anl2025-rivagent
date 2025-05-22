84f5fed33a1249bc41bb49ba6320e0c49c3b8b6c
kawahara <kawahara@katfuji.lab.tuat.ac.jp>
2025-05-22 14:00:14 +0900

    feat: 現在の効用値に、次の交渉の効用値も考慮するようにした

--- rivagent/rivagent.py
+++ rivagent/rivagent.py
@@ -15,7 +15,9 @@ import numpy as np
 from negmas.outcomes import Outcome
 
 #be careful: When running directly from this file, change the relative import to an absolute import. When submitting, use relative imports.
-#from helpers.helperfunctions import set_id_dict, ...
+from .helpers.helperfunctions import get_current_negotiation_index, get_agreement_at_index, get_outcome_space_from_index \
+    , get_agreement_at_index, get_nmi_from_index, did_negotiation_end, set_id_dict \
+    , get_number_of_subnegotiations
 
 from anl2025.negotiator import ANL2025Negotiator
 from negmas.sao.controllers import SAOController, SAOState
@@ -26,212 +28,270 @@ from negmas import (
 )
 
 class UtilArea:
-    def __init__(self, min_u, max_u, all_bids):
-        self.min_u = min_u
-        self.u_size = max_u - min_u
-        self.max_u = max_u
+    def __init__(self, min_zu, max_zu, outcomes):
+        self.min_zu = min_zu
+        self.zu_size = max_zu - min_zu
+        self.max_zu = max_zu
 
-        self.all_bids = all_bids
-        self.n_bids = len(all_bids)
+        self.outcomes = outcomes
+        self.n_outcomes = len(outcomes)
     
         self.min_ru = None
         self.max_ru = None
 
-class UtilModel:
-    def __init__(self, ufun, all_bids):
-        self.ufun = ufun
-        self.all_bids = all_bids
-        self.n_bids = len(all_bids)
-        self._bid2u = {str(bid):ufun(bid) for bid in self.all_bids}
-        self._bid2u = {k:v for k,v in sorted(self._bid2u.items(), key=lambda x:x[1])}
-
-        n_area_splits = 5
+class UtilSpace:
+    def __init__(self, side_ufun, ufun, outcomes, agreements, neg_index, neg_num):
+        self.outcomes = outcomes
+        self.__init_outcome2zu(side_ufun, ufun, outcomes, agreements, neg_index, neg_num, risk_param=0.0)
+        self.__init_areas(outcomes, n_area_splits=5)
+    
+    def __init_outcome2zu(self, side_ufun, ufun, outcomes, agreements, neg_index, neg_num, risk_param):
+        self.outcome2zu = {}
+        if neg_index == neg_num - 1:
+            for o in outcomes:
+                self.outcome2zu[str(o)] = side_ufun(o)
+        else:
+            # print(f'---- <neg_index={neg_index}> ----')
+            for o in outcomes:
+                next_accept_utils = []
+                next_end_neg_util = None
+                for no in outcomes:
+                    bid = tuple(agreements + [o, no] + [None]*(neg_num-neg_index-2))
+                    util = ufun(bid)
+                    if no is None:
+                        next_end_neg_util = util
+                    else:
+                        next_accept_utils.append(util)
+                next_accept_zu = np.mean(next_accept_utils) - risk_param * np.std(next_accept_utils)
+                zu = max(next_accept_zu, next_end_neg_util)
+                # print(f'-- {o} --')
+                # print(f'mean: {np.mean(next_accept_utils)}, std: {np.std(next_accept_utils)} ← {next_accept_utils}')
+                # print(f'next_accept_zu<{next_accept_zu}> = mean<{np.mean(next_accept_utils)}> - risk_param<{risk_param}> * std<{np.std(next_accept_utils)}>]')
+                # print(f'zu<{zu}> = max(accept_zu<{next_accept_zu}>, end_neg_u<{next_end_neg_util}>)')
+                self.outcome2zu[str(o)] = zu
+        self.outcome2zu = {k:v for k,v in \
+            sorted(self.outcome2zu.items(), key=lambda x:x[1])}
+        # exit()
+    
+    def __init_areas(self, outcomes, n_area_splits):
         area_size = 1.0 / n_area_splits
 
-        bids_tmp = copy(all_bids)
-        self._areas = []
+        outcome_tmp = copy(outcomes)
+        self.areas = []
         for i in range(n_area_splits-1,-1,-1):
-            min_u = area_size * i
+            min_zu = area_size * i
 
-            bids_in_area = [bid for bid in bids_tmp if self.get(bid) >= min_u]
-
-            for bid in bids_in_area:
-                bids_tmp.remove(bid)
-
-            if len(bids_in_area) == 0:
+            outcomes_in_area = []
+            for o in outcome_tmp:
+                if self.get_zu_from_outcome(o) >= min_zu:
+                    outcomes_in_area.append(o)
+            for o in outcomes_in_area:
+                outcome_tmp.remove(o)
+            
+            if len(outcomes_in_area) == 0:
                 continue
 
             if self.n_areas == 0:
-                max_u = None
-                for bid in bids_in_area:
-                    u = self.get(bid)
-                    if (max_u is None) or u > max_u:
-                        max_u = u
+                max_zu = None
+                for o in outcomes_in_area:
+                    u = self.get_zu_from_outcome(o)
+                    if (max_zu is None) or u > max_zu:
+                        max_zu = u
+                max_zu = np.max([self.get_zu_from_outcome(o)
+                    for o in outcomes_in_area])
             else:
-                max_u = area_size * (i+1)
+                max_zu = area_size * (i+1)
             
-            ua = UtilArea(min_u, max_u, bids_in_area)
-            self._areas.insert(0, ua)
+            area = UtilArea(min_zu, max_zu, outcomes_in_area)
+            self.areas.insert(0, area)
         
-        total_u_size = sum([area.u_size for area in self._areas], 0)
+        total_zu_size = sum([area.zu_size for area in self.areas], 0)
         
-        self._areas[0].min_ru = 0.0
-        self._areas[self.n_areas-1].max_ru = 1.0
+        self.areas[0].min_ru = 0.0
+        self.areas[self.n_areas-1].max_ru = 1.0
         for i in range(self.n_areas-1):
-            r = self._areas[i].min_ru + (self._areas[i].u_size / total_u_size)
-            self._areas[i].max_ru = r
-            self._areas[i+1].min_ru = r
+            r = self.areas[i].min_ru + (self.areas[i].zu_size / total_zu_size)
+            self.areas[i].max_ru = r
+            self.areas[i+1].min_ru = r
     
-    def get(self, bid):
-        return self._bid2u[str(bid)]
+    def get_zu_from_outcome(self, outcome):
+        return self.outcome2zu[str(outcome)]
     
-    def get_bid_near_threshold(self, threshold):
-        for bid in self.all_bids:
-            if self.get(bid) >= threshold:
-                return bid
+    def get_outcome_near_threshold(self, threshold):
+        for o in self.outcomes:
+            if self.get_zu_from_outcome(o) >= threshold:
+                return o
         else:
-            return self._bid2u[self._bid2u.keys()[-1]]
+            return self.outcome2zu[self.outcome2zu.keys()[-1]]
     
-    def calc_by_relative(self, relative):
+    def calc_zu_by_ratio(self, relative):
         for i in range(self.n_areas-1,-1,-1):
-            min_ru = self._areas[i].min_ru
+            min_ru = self.areas[i].min_ru
             if relative >= min_ru:
-                r_in_area = (relative - min_ru) / (self._areas[i].max_ru - min_ru)
-                return self._areas[i].u_size * r_in_area + self._areas[i].min_u
+                r_in_area = (relative - min_ru) / (self.areas[i].max_ru - min_ru)
+                return self.areas[i].zu_size * r_in_area + self.areas[i].min_zu
     
     def calc_std(self):
-        return np.std(list(self._bid2u.values()))
+        return np.std(list(self.outcome2zu.values()))
 
     def calc_max_dist(self):
-        all_us = list(self._bid2u.values())
-        return np.max(all_us) - np.min(all_us)
+        all_zus = list(self.outcome2zu.values())
+        return np.max(all_zus) - np.min(all_zus)
     
     @property
     def n_areas(self):
-        return len(self._areas)
-    
-    def get_area(self, i):
-        return self._areas[i]
+        return len(self.areas)
 
 class OpponentModel:
-    def __init__(self, u_model, n_steps):
-        self._c1 = 1.3
-        self._c2 = 1.3
-        self._c3 = 0.2
+    def __init__(self, util_space, n_steps):
+        self.c1 = 1.3
+        self.c2 = 1.3
+        self.c3 = 0.2
 
-        self._max_dist = u_model.calc_max_dist()
-        self._set_difficulty(u_model.calc_std())
-        self._n_smooth_max = int(n_steps * 0.1)
+        self.util_space = util_space
 
-        self._u_get_fn = u_model.get
-        self._curr_my_u = None
-        self._u_dists = []
+        self.max_dist = util_space.calc_max_dist()
+        self.__init_difficulty(util_space)
+        self.n_smooth_max = int(n_steps * 0.1)
+
+        self.current_my_zu = None
+        self.zu_dists = []
     
-    def _set_difficulty(self, my_u_std):
-        if my_u_std < 0.15:
-            self._difficulty = self._c2
-        elif my_u_std < 0.3:
-            self._difficulty = 1.0
+    def __init_difficulty(self, util_space):
+        my_zu_std = util_space.calc_std()
+        if my_zu_std < 0.15:
+            self.difficulty = self.c2
+        elif my_zu_std < 0.3:
+            self.difficulty = 1.0
         else:
-            self._difficulty = 1 / self._c2
+            self.difficulty = 1 / self.c2
     
-    def update_when_proposal(self, my_bid):
-        self._curr_my_u = self._u_get_fn(my_bid)
+    def update_when_proposal(self, my_outcome):
+        self.current_my_zu = self.util_space.get_zu_from_outcome(my_outcome)
     
-    def update_when_respond(self, opponent_bid):
-        opponent_u = self._u_get_fn(opponent_bid)
-        self._u_dists.append(abs(opponent_u - self._curr_my_u))
+    def update_when_respond(self, opponent_outcome):
+        opponent_zu = self.util_space.get_zu_from_outcome(opponent_outcome)
+        self.zu_dists.append(abs(opponent_zu - self.current_my_zu))
     
-    def _calc_n_smooth(self, step):
-        return int(min(step * 0.5, self._n_smooth_max))
+    def calc_n_smooth(self, step):
+        return int(min(step * 0.5, self.n_smooth_max))
     
-    def _calc_alpha(self, step):
-        recent_d_std = np.std(self._u_dists[-self._n_smooth_max:])
+    def calc_alpha(self, step):
+        recent_d_std = np.std(self.zu_dists[-self.n_smooth_max:])
         if recent_d_std < 0.05:
-            return 0.5 + self._c3
+            return 0.5 + self.c3
         elif recent_d_std < 0.2:
             return 0.5
         else:
-            return 0.5 - self._c3
+            return 0.5 - self.c3
     
     def calc_concession(self, state):
         if state.relative_time < 0.1:
             return 2.0
         
-        start_dist_mean = np.mean(self._u_dists[-self._calc_n_smooth(state.step):])
-        recent_dist_mean = np.mean(self._u_dists[-self._calc_n_smooth(state.step)*2:])
-        alpha = self._calc_alpha(state.step)
+        start_dist_mean = np.mean(self.zu_dists[-self.calc_n_smooth(state.step):])
+        recent_dist_mean = np.mean(self.zu_dists[-self.calc_n_smooth(state.step)*2:])
+        alpha = self.calc_alpha(state.step)
 
         dist_slope = (start_dist_mean - recent_dist_mean) / (start_dist_mean + 1e-6)
-        curr_dist_ratio = 1 - recent_dist_mean / self._max_dist
+        curr_dist_ratio = 1 - recent_dist_mean / self.max_dist
         concession_tmp = alpha * dist_slope + (1-alpha) * curr_dist_ratio
 
-        return self._c1 ** (concession_tmp*2-1) * self._difficulty
+        # return self.c1 ** (concession_tmp*2-1) * self.difficulty
+        return 1.0
 
 class Threshold:
-    def __init__(self, u_model, opponent_model):
-        self._u_model = u_model
-        self._opponent_model = opponent_model
+    def __init__(self, util_space, opponent_model):
+        self.util_space = util_space
+        self.opponent_model = opponent_model
     
     def calc(self, state):
-        relative = 1 - state.relative_time ** self._opponent_model.calc_concession(state)
-        return self._u_model.calc_by_relative(relative)
+        ratio = 1 - state.relative_time ** self.opponent_model.calc_concession(state)
+        return self.util_space.calc_zu_by_ratio(ratio)
 
-class SubNegotiator:
-    def __init__(self, sub_negotiation, all_bids):
-        _neg_info, _cntxt = sub_negotiation
-        self._u_model = UtilModel(_cntxt['ufun'], all_bids)
-        self._opponent_model = OpponentModel(self._u_model, _neg_info.nmi.n_steps)
-        self._threshold = Threshold(self._u_model, self._opponent_model)
+class SideNegotiatorStrategy:
+    def __init__(self, main_negotiator, negid, neg_index):
+        self.util_space = UtilSpace(
+            side_ufun = main_negotiator.negotiators[negid].context['ufun'],
+            ufun = main_negotiator.ufun, 
+            outcomes = get_outcome_space_from_index(main_negotiator, neg_index),
+            agreements = [get_agreement_at_index(main_negotiator,i) 
+                for i in range(neg_index)],
+            neg_index = neg_index,
+            neg_num = get_number_of_subnegotiations(main_negotiator),
+        )
+        self.opponent_model = OpponentModel(
+            util_space = self.util_space,
+            n_steps = get_nmi_from_index(main_negotiator, neg_index).n_steps,
+        )
+        self.threshold = Threshold(
+            util_space = self.util_space, 
+            opponent_model = self.opponent_model,
+        )
     
     def proposal(self, state):
-        th = self._threshold.calc(state)
-        target_bid = self._u_model.get_bid_near_threshold(th)
-        self._opponent_model.update_when_proposal(target_bid)
-        return target_bid
+        th = self.threshold.calc(state)
+        target_outcome = self.util_space.get_outcome_near_threshold(th)
+        self.opponent_model.update_when_proposal(target_outcome)
+        return target_outcome
 
     def respond(self, state):
-        self._opponent_model.update_when_respond(state.current_offer)
-        my_util = self._u_model.get(state.current_offer)
-        th = self._threshold.calc(state)
-        if my_util >= th:
+        self.opponent_model.update_when_respond(state.current_offer)
+        th = self.threshold.calc(state)
+        if th <= self.util_space.get_zu_from_outcome(None):
+            return ResponseType.END_NEGOTIATION
+        
+        opponent_zu = self.util_space.get_zu_from_outcome(state.current_offer)
+        if opponent_zu >= th:
             return ResponseType.ACCEPT_OFFER
         return ResponseType.REJECT_OFFER
 
 class RivAgent(ANL2025Negotiator):
     def init(self):
-        self.sub_negs = []
-    
-    @property
-    def curr_sub_neg_idx(self):
-        return len(self.sub_negs) - 1
+        self.id_dict = {}
+        set_id_dict(self)
+
+        self.current_neg_index = -1
+        self.side_neg_strategies = []
 
     @property
-    def curr_sub_neg(self):
-        return self.sub_negs[self.curr_sub_neg_idx]
-    
-    def _update(self, negotiator_id: str, state: SAOState):
-        if self.curr_sub_neg_idx < len(self.finished_negotiators):
-            self.sub_negs.append(
-                SubNegotiator(
-                    sub_negotiation = self.negotiators[negotiator_id],
-                    all_bids = self.ufun.outcome_spaces[self.curr_sub_neg_idx].enumerate_or_sample(), 
-                )
-            )
+    def current_side_neg_strategy(self):
+        return self.side_neg_strategies[self.current_neg_index]
 
     def propose(
             self, negotiator_id: str, state: SAOState, dest: str | None = None
     ) -> Outcome | None:
-        self._update(negotiator_id, state)
-        return self.curr_sub_neg.proposal(state)
+        if did_negotiation_end(self):
+            self.side_neg_strategies.append(
+                SideNegotiatorStrategy(self, negotiator_id, get_current_negotiation_index(self))
+            )
+        return self.current_side_neg_strategy.proposal(state)
 
     def respond(
             self, negotiator_id: str, state: SAOState, source: str | None = None
     ) -> ResponseType:
-        self._update(negotiator_id, state)
-        return self.curr_sub_neg.respond(state)
+        ret = self.current_side_neg_strategy.respond(state)
+
+        # from .helpers.helperfunctions import get_nmi_from_index
+        # if ret != ResponseType.REJECT_OFFER or state.step == get_nmi_from_index(self, self.current_neg_index).n_steps:
+        #     agreements, bid = [], []
+        #     for i in range(get_number_of_subnegotiations(self)):
+        #         if i > self.current_neg_index:
+        #             bid.append(None)
+        #             continue
+
+        #         if i < self.current_neg_index:
+        #             outcome = get_agreement_at_index(self, i)
+        #         else:
+        #             outcome = state.current_offer
+        #         agreements.append(outcome)
+        #         bid.append(outcome)
+        #     print(f'[{self.current_neg_index+1}/{get_number_of_subnegotiations(self)}]', bid, self.ufun(tuple(bid)))
+
+        return ret
 
 if __name__ == "__main__":
-    from .helpers.runner import run_negotiation, visualize
-    results = run_negotiation()
-    visualize(results)
+    from .helpers.runner import run_negotiation, run_for_debug, visualize
+    # run_for_debug(RivAgent, small=True)
+    results = run_negotiation(RivAgent)
+    # visualize(results)
